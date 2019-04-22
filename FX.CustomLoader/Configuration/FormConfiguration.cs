e3f2a875163e600915135f55d1b4968208ffa53e
Ryan Farley <ryan@farleyzone.com>
2019-04-22 14:57:58 -0700

    Added new support for globally adding SmartParts

---
+++ FX.CustomLoader/Configuration/FormConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using FX.CustomLoader.Utility;
+
+namespace FX.CustomLoader.Configuration
+{
+    internal class FormConfiguration
+    {
+        public string ID { get; set; }
+        public string SmartPart { get; set; }
+        public string Title { get; set; }
+        public string Workspace { get; set; }
+        public string ShowInMode { get; set; }
+
+        public static FormConfiguration LoadFromFile(string FileName)
+        {
+            if (!File.Exists(FileName)) return null;
+            return FormConfiguration.LoadFromJson(File.ReadAllText(FileName));
+        }
+
+        public static FormConfiguration LoadFromJson(string Json)
+        {
+            return Json.FromJson<FormConfiguration>();
+        }
+    }
+}
--- FX.CustomLoader/Module.cs
+++ FX.CustomLoader/Module.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using Sage.Platform.Application;
+using Sage.Platform.Application.UI;
+using Sage.Platform.WebPortal.Workspaces;
+using Sage.Platform.WebPortal.Workspaces.Tab;
+using FX.CustomLoader.Configuration;
 
 namespace FX.CustomLoader
 {
     public class Module : Sage.Platform.Application.IModule
     {
+        [ServiceDependency(Type = typeof(WorkItem))]
+        public UIWorkItem PageWorkItem { get; set; }
+
         public Module()
         {
             this.ScriptFolder = "~/Custom/Scripts";
             this.ModulesFolder = "~/Custom/Modules";
             this.StyleFolder = "~/Custom/Style";
+            this.FormsFolder = "~/Custom/Forms";
         }
 
         [DisplayName("Script Folder")]
@@ -29,6 +39,34 @@ namespace FX.CustomLoader
         [Description("The root folder where custom CSS style files are located. Default value is ~/Custom/Style")]
         public string StyleFolder { get; set; }
 
+        [DisplayName("Forms Folder")]
+        [Description("The root folder where SmartPart definition files are located. Default value is ~/Custom/Forms")]
+        public string FormsFolder { get; set; }
+
+        private MainContentWorkspace _mainworkspace = null;
+        private MainContentWorkspace MainContent
+        {
+            get { return _mainworkspace ?? (_mainworkspace = PageWorkItem.Workspaces["MainContent"] as MainContentWorkspace); }
+        }
+
+        private TabWorkspace _tabworkspace = null;
+        private TabWorkspace TabWorkspace
+        {
+            get { return _tabworkspace ?? (_tabworkspace = PageWorkItem.Workspaces["TabControl"] as TabWorkspace); }
+        }
+
+        private TaskPaneWorkspace _taskworkspace = null;
+        private TaskPaneWorkspace TaskPane
+        {
+            get { return _taskworkspace ?? (_taskworkspace = PageWorkItem.Workspaces["TaskPane"] as TaskPaneWorkspace); }
+        }
+
+        private DialogWorkspace _dialogworkspace = null;
+        private DialogWorkspace DialogWorkspace
+        {
+            get { return _dialogworkspace ?? (_dialogworkspace = PageWorkItem.Workspaces["DialogWorkspace"] as DialogWorkspace); }
+        }
+
         public void Load()
         {
             // do not load on dialog pages (such as groupbuilder.aspx)
@@ -37,6 +75,7 @@ namespace FX.CustomLoader
             this.LoadModules();
             this.LoadScripts();
             this.LoadStyles();
+            this.LoadForms();
         }
 
         private void LoadModules()
@@ -79,13 +118,58 @@ namespace FX.CustomLoader
             }
         }
 
-        public List<string> GetFiles(string VirtualPath, string Extension)
+        /*
+         * Sample Config (can be named anything.json)
+         * {
+         *      "ID": "mySmartPart",
+         *      "SmartPart": "~/SmartParts/Account/MySmartPart",
+         *      "Workspace": "DialogWorkspace",
+         *      "Title": "Title (optional)",
+         *      "ShowInMode": ""
+         * }
+         */
+        private void LoadForms()
+        {
+            if (CurrentPage == null) return;
+
+            var files = GetFiles(FormsFolder, "json");
+            foreach (var file in files)
+            {
+                var config = FormConfiguration.LoadFromFile(file);
+                if (config != null)
+                {
+                    if (string.IsNullOrEmpty(config.SmartPart) || string.IsNullOrEmpty(config.ID) || string.IsNullOrEmpty(config.Workspace)) continue;
+                    if (!File.Exists(HttpContext.Current.Server.MapPath(config.SmartPart))) continue;
+
+                    Control control;
+                    try
+                    {
+                        control = CurrentPage.LoadControl(config.SmartPart);
+                    }
+                    catch (Exception ex)
+                    {
+                        control = new LiteralControl(ex.Message);
+                    }
+
+                    control.ID = config.ID;
+                    if (string.IsNullOrEmpty(config.Title)) config.Title = config.ID;
+                    GetWorkspaceFromName(config.Workspace).Show(control, new SmartPartInfo(config.Title, config.Title));
+                }
+            }
+        }
+
+        private IWorkspace GetWorkspaceFromName(string WorkspaceName)
+        {
+            return (IWorkspace)this.GetType().GetProperty(WorkspaceName, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this, null);
+        }
+
+        private List<string> GetFiles(string VirtualPath, string Extension)
         {
             if (string.IsNullOrEmpty(VirtualPath) || !Directory.Exists(GetServerPath(VirtualPath))) return new List<string>();
             return Directory.GetFiles(GetServerPath(VirtualPath), "*." + Extension, SearchOption.AllDirectories).ToList();
         }
 
-        public List<string> GetSubDirectories(string VirtualPath)
+        private List<string> GetSubDirectories(string VirtualPath)
         {
             if (string.IsNullOrEmpty(VirtualPath) || !Directory.Exists(GetServerPath(VirtualPath))) return new List<string>();
             return Directory.GetDirectories(GetServerPath(VirtualPath), "*", SearchOption.TopDirectoryOnly).ToList();
